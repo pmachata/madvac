d9c2bd5a12892a77119e12a4ace7662deb414e5d
Petr Machata <pmachata@gmail.com>
2018-06-10 02:06:23 +0200

    src/field: A new module for class Field

--- src/madvac.js
+++ src/madvac.js
@@ -1,77 +1,9 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import { Field } from './field.js';
 
 function randint(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-class Field {
-    constructor(game, x, y, id) {
-        this.game = game;
-        this.covered = true;
-        this.flagged = false;
-        this.hasMine = false;
-        this.x = x;
-        this.y = y;
-        this.id = id;
-    }
-
-    uncover() {
-        if (!this.game.started) {
-            this.game.start();
-        }
-        if (!this.flagged && this.covered) {
-            this.covered = false;
-            if (this.countNeighMines() == 0) {
-                this.uncoverNeighbors();
-            }
-        }
-        if (this.hasMine) {
-            this.game.kaboom();
-        }
-    }
-
-    toggle() {
-            if (this.covered) {
-                this.uncover();
-            } else {
-                this.covered = true;
-            }
-    }
-
-    flag() {
-        if (this.covered) {
-            this.flagged = !this.flagged;
-        }
-    }
-
-    neighbor(dx, dy) {
-        return this.game.field(this.x + dx, this.y + dy);
-    }
-
-    countNeighMines() {
-        var count = 0;
-        for (var dx = -1; dx <= 1; ++dx) {
-            for (var dy = -1; dy <= 1; ++dy) {
-                var neigh = this.neighbor(dx, dy);
-                if (neigh && neigh.hasMine) {
-                    ++count;
-                }
-            }
-        }
-        return count;
-    }
-
-    uncoverNeighbors() {
-        for (var dx = -1; dx <= 1; ++dx) {
-            for (var dy = -1; dy <= 1; ++dy) {
-                var neigh = this.neighbor(dx, dy);
-                if (neigh) {
-                    neigh.uncover();
-                }
-            }
-        }
-    }
-};
 
 class Game {
     constructor() {
