02642cee8f98935325d67573443131786745d7b3
Petr Machata <pmachata@gmail.com>
2018-06-19 23:42:00 +0200

    Gen: Generate deterministic games

--- src/gen.js
+++ src/gen.js
@@ -1,3 +1,5 @@
+import { detPlay } from './det.js';
+
 function randint(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -18,13 +20,35 @@ function randEmptyField(board) {
 
 function genGame(board, want_nmines, x0, y0) {
     var origObserver = board.setFieldObserver(null);
-    for (var nmines = 0; nmines < want_nmines; ) {
+
+    var mines = [];
+    var tally = [0];
+    while (mines.length < want_nmines) {
         var field = randEmptyField(board);
-        if (field.x !== x0 || field.y !== y0) {
-            field.hasMine = true;
-            ++nmines;
+
+        field.hasMine = true;
+        mines.push(field);
+
+        var playable = detPlay(board, x0, y0);
+        board.allFields().forEach(function(field) {
+            field.covered = true;
+            field.flagged = false;
+        });
+        if (playable) {
+            tally.push(0);
+            continue;
+        }
+
+        field = mines.pop();
+        field.hasMine = false;
+
+        while (tally.length > 1 && ++tally[tally.length - 1] > 3) {
+            tally.pop();
+            field = mines.pop();
+            field.hasMine = false;
         }
     }
+
     board.setFieldObserver(origObserver);
 }
 
