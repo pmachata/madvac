d3aabe247ffe52b26c1e66f24ad34335a8276cda
Petr Machata <pmachata@gmail.com>
2018-07-14 18:14:51 +0200

    Drop csp.js, set128.js and related

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,9 @@ const path = require('path');
 module.exports = {
     entry: {
         madvac: './src/madvac.js',
-        'csp-tests': './src/csp-tests.js',
         'det-tests': './src/det-tests.js',
         'gen-tests': './src/gen-tests.js',
         'set128-asm-tests': './src/set128-asm-tests.js',
-        'set128-tests': './src/set128-tests.js',
         'cons-asm-tests': './src/cons-asm-tests.js',
         'csp-asm-tests': './src/csp-asm-tests.js',
     },
