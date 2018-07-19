1d3983fb3423b3b146fbd0e2ee0a0d20d2571383
Petr Machata <pmachata@gmail.com>
2018-07-19 23:38:07 +0200

    gen-tests: Test that generated boards are playable

--- src/gen-tests.js
+++ src/gen-tests.js
@@ -1,6 +1,11 @@
 import { assert } from 'chai';
 import { genGame } from './gen.js';
 import { Board } from './board.js';
+import { assertPlayable, assertNotPlayable } from './tests.js';
 
-var board = new Board(11, 11);
-genGame(board, 30, 5, 5);
+for (let i = 0; i < 10; ++i) {
+    let board = new Board(11, 11);
+    let c0 = [5, 5];
+    genGame(board, 30, ...c0);
+    assertPlayable("", board, ...c0);
+}
