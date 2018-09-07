7834366945e8c10972290a2c142dd90676ff8bf6
Petr Machata <pmachata@gmail.com>
2018-09-08 01:34:20 +0200

    gen-tests: Add a gen-test with deterministic coords
    This is going to be useful for performance measurements.

--- src/gen-tests.js
+++ src/gen-tests.js
@@ -3,9 +3,29 @@ import { genGame } from './gen.js';
 import { Board } from './board.js';
 import { assertPlayable, assertNotPlayable } from './tests.js';
 
+function deterministicRandCoord() {
+    let x = 0;
+    let y = 0;
+    return function (w, h) {
+        ++x;
+        if (x >= w) {
+            x = 0;
+            ++y;
+        }
+        return [x, y];
+    }
+}
+
 for (let i = 0; i < 10; ++i) {
-    let board = new Board(11, 11);
+    let board = new Board(10, 10);
     let c0 = [5, 5];
     genGame(board, 30, ...c0);
+    assertPlayable("", board, ...c0);
+}
+
+for (let i = 0; i < 10; ++i) {
+    let board = new Board(10, 10);
+    let c0 = [5, 5];
+    genGame(board, 38, ...c0, deterministicRandCoord());
     assertPlayable("", board, ...c0);
 }
