1c6f0c0e98d1dd4fd3b07d852b6443dc10366761
Petr Machata <pmachata@gmail.com>
2018-09-18 01:32:36 +0200

    det: Pass to DetGame a field, not coordinates
    Passing in a field is more handy for autoplay, where a game is already
    in progress and one wishes to make an incremental solution step.

--- src/det.js
+++ src/det.js
@@ -2,12 +2,15 @@ import { Board } from './board.js';
 import { asm, heap, logger } from './asmctx.js';
 
 class DetGame {
-    constructor(board, x0, y0) {
+    constructor(board, field) {
         this.board = board;
         this.csp = heap.allocaCsp();
         asm.csp_init(this.csp);
-        this.uncoverQueue = [board.field(x0, y0)];
 
+        this.uncoverQueue = [];
+        if (field) {
+            this.uncoverQueue.push(field);
+        }
         this.origObserver = this.board.setFieldObserver(this);
     }
 
@@ -80,13 +83,12 @@ class DetGame {
     }
 };
 
-// Play the game deterministically with the help of CSP.
-function detPlay(board, x0, y0) {
+function play(board, field0) {
     var ret;
 
     heap.enter();
     {
-        var game = new DetGame(board, x0, y0);
+        var game = new DetGame(board, field0);
         while (game.step()) {
         }
         game.resetObserver();
@@ -96,6 +98,12 @@ function detPlay(board, x0, y0) {
     heap.leave();
 
     return ret;
+}
+
+// Play the game deterministically with the help of CSP.
+function detPlay(board, x0, y0) {
+    var field0 = board.field(x0, y0);
+    return play(board, field0);
 }
 
 export { detPlay };
